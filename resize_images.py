c675017155ccc067b29385125b1d7d546e933085
gregwchase <gregory.chase@colorado.edu>
2017-09-12 17:27:16 -0600

    Added create_directory function (complete)

--- resize_images.py
+++ resize_images.py
@@ -1,10 +1,12 @@
 import os
 import sys
 from PIL import Image
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-# def create_directory(directory):
-#     if not os.path.exists(directory):
-#         os.makedirs(directory)
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def resize_images(path, new_path):
     for item in dirs:
@@ -12,13 +14,20 @@ def resize_images(path, new_path):
             img = Image.open(path+item)
             f, e = os.path.splitext(path+item)
             img_resize = img.resize((120,120), Image.ANTIALIAS)
-            new_path = 'sample-resized/'
-            # img_resize.save(f + '_resized.jpeg', 'JPEG')
-            img_resize.save(os.path.join(new_path,item))
+            created_path = new_path
+            img_resize.save(os.path.join(created_path,item))
 
 
 if __name__ == '__main__':
-    file_path = "sample/"
-    new_path = "sample-resized/"
+    # Specify file paths
+    file_path = "train_100/"
+    new_path = "train_100_resized/"
+
+    # Create the directory if it doesn't exist
+    create_directory(new_path)
+
+    # Creates a list of file strings
     dirs = os.listdir(file_path)
+
+    # Resize the images, and save to the new directory
     resize_images(path = file_path, new_path = new_path)
