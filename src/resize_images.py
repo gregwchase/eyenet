67098550db162a00f9952633200e1fe8eb5d752c
gregwchase <gregory.chase@colorado.edu>
2017-09-17 12:46:34 -0600

    Added img_size paramter to resize_images; Added comments for functions.

--- src/resize_images.py
+++ src/resize_images.py
@@ -3,29 +3,29 @@ import sys
 from PIL import Image
 from PIL import ImageFile
 # ImageFile.LOAD_TRUNCATED_IMAGES = True
-import cv2
 
 def create_directory(directory):
     '''
     Creates a new folder in the specified directory if the folder doesn't exist.
 
     INPUT
-        directory: Folder to be created, called as "folder/"
+        directory: Folder to be created, called as "folder/".
 
     OUTPUT
-        New folder in the current directory
+        New folder in the current directory.
     '''
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-def resize_images(path, new_path):
+def resize_images(path, new_path, img_size):
     '''
     Resizes images based on the specified dimensions.
-    Images are saves to the new_path directory
+    Images are saves to the new_path directory.
 
     INPUT
         path: Path where the current, unscaled images are contained.
-        new_path: Path to save the resized images
+        new_path: Path to save the resized images.
+        img_size: New size for the rescaled images.
 
     OUTPUT
         All images resized and saved from the old folder to the new folder.
@@ -37,7 +37,7 @@ def resize_images(path, new_path):
         if os.path.isfile(path+item) and not item.startswith('.'):
             img = Image.open(path+item)
             f, e = os.path.splitext(path+item)
-            img_resize = img.resize((120,120), Image.ANTIALIAS)
+            img_resize = img.resize((img_size,img_size), Image.ANTIALIAS)
             created_path = new_path
             img_resize.save(os.path.join(created_path,item))
 
@@ -53,5 +53,6 @@ def resize_images_cv(path, new_path):
 
 
 if __name__ == '__main__':
-    resize_images(path = '../data/train', new_path='../data/train-resized')
-    resize_images(path = '../data/test', new_path='../data/test-resized')
+    resize_images(path = "../sample/", new_path='../sample-resized/', img_size=120)
+    # resize_images(path = '../data/train', new_path='../data/train-resized')
+    # resize_images(path = '../data/test', new_path='../data/test-resized')
