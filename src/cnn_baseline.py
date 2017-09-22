5e8a60fb7d17aa6da8c25f3abb536a0c6e531c80
gregwchase <gregory.chase@colorado.edu>
2017-09-22 12:04:20 -0600

    Changed optimizer from Adam to SGD.

--- src/cnn_baseline.py
+++ src/cnn_baseline.py
@@ -86,8 +86,10 @@ model.add(Dropout(0.15))
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 model.add(BatchNormalization())
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss = 'categorical_crossentropy',
-    optimizer='adam',
+    optimizer=sgd, #'adam'
     metrics=['accuracy'])
 
 earlyStopping = EarlyStopping(monitor='acc', #val_loss
