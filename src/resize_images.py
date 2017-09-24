17e49dcd6134ff5954b81e86ddcb766fef342a45
gregwchase <gregory.chase@colorado.edu>
2017-09-23 19:52:25 -0600

    Added crop_and_resize_images function

--- src/resize_images.py
+++ src/resize_images.py
@@ -1,8 +1,13 @@
 import os
 import sys
-from PIL import Image
+# from PIL import Image
 from PIL import ImageFile
-# ImageFile.LOAD_TRUNCATED_IMAGES = True
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+from skimage import io
+from skimage.transform import resize
+import numpy as np
+import time
+
 
 def create_directory(directory):
     '''
@@ -41,18 +46,55 @@ def resize_images(path, new_path, img_size):
             created_path = new_path
             img_resize.save(os.path.join(created_path,item))
 
-def resize_images_cv(path, new_path):
+# def resize_images_cv(path, new_path):
+#     for item in dirs:
+#         if os.path.isfile(path+item) and not item.startswith('.'):
+#             img = Image.open(path+item)
+#             f, e = os.path.splitext(path+item)
+#             img_resize = cv2.resize(img, (120, 120), interpolation = cv2.INTER_AREA)
+#             # img_resize = img.resize((120,120), Image.ANTIALIAS)
+#             created_path = new_path
+#             img_resize.save(os.path.join(created_path,item))
+
+
+def crop_and_resize_images(path, new_path, cropx, cropy, img_size=256):
+    '''
+    Crops, resizes, and stores all images from a directory in a new directory.
+
+    INPUT
+        path: Path where the current, unscaled images are contained.
+        new_path: Path to save the resized images.
+        img_size: New size for the rescaled images.
+
+    OUTPUT
+        All images cropped, resized, and saved from the old folder to the new folder.
+    '''
+    create_directory(new_path)
+    dirs = [l for l in os.listdir(path) if l != '.DS_Store']
+    total = 0
+
     for item in dirs:
-        if os.path.isfile(path+item) and not item.startswith('.'):
-            img = Image.open(path+item)
-            f, e = os.path.splitext(path+item)
-            img_resize = cv2.resize(img, (120, 120), interpolation = cv2.INTER_AREA)
-            # img_resize = img.resize((120,120), Image.ANTIALIAS)
-            created_path = new_path
-            img_resize.save(os.path.join(created_path,item))
+        # img = io.imread('../data/sample/' + '13_left.jpeg')
+        img = io.imread(path+item)
+        # f,e = os.path.splitext(path+item)
+        y,x,channel = img.shape
+        startx = x//2-(cropx//2)
+        starty = y//2-(cropy//2)
+        img = img[starty:starty+cropy,startx:startx+cropx]
+        img = resize(img, (256,256))
+        io.imsave(str(new_path + item), img)
+        total += 1
+        print("Saving: ", item, total)
 
 
 if __name__ == '__main__':
-    resize_images(path = "../sample/", new_path='../sample-resized/', img_size=120)
+    start_time = time.time()
+    # resize_images(path = "../sample/", new_path='../sample-resized/', img_size=120)
     # resize_images(path = '../data/train/', new_path='../data/train-resized/')
     # resize_images(path = '../data/test/', new_path='../data/test-resized/')
+
+    # crop_and_resize_images(path='../data/sample/', new_path='../data/sample-resized-256-1800/', cropx=1800, cropy=1800, img_size=256)
+    crop_and_resize_images(path='../data/train/', new_path='../data/train-resized-256/', cropx=1800, cropy=1800, img_size=256)
+    crop_and_resize_images(path='../data/test/', new_path='../data/test-resized-256/', cropx=1800, cropy=1800, img_size=256)
+    # crop_and_resize_images(path='../train_100/', new_path='../data/train_100-resized-256-1800/', cropx=1800, cropy=1800, img_size=256)
+    print(time.time() - start_time)
