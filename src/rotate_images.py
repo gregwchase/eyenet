341f4db751ab1cd547bc895024e0588991e1e9c0
gregwchase <gregory.chase@colorado.edu>
2017-09-24 18:55:36 -0600

    Completed mirror_images function

--- src/rotate_images.py
+++ src/rotate_images.py
@@ -3,6 +3,7 @@ import numpy as np
 from skimage import io
 from skimage.transform import rotate
 import cv2
+import os
 
 def rotate_image(file_path, degrees_of_rotation):
     '''
@@ -22,21 +23,24 @@ def rotate_image(file_path, degrees_of_rotation):
     io.imsave('img_name + _r_ + 90/180 + .jpeg', img)
     pass
 
-def mirror_images(file_path):
+def mirror_images(file_path, mirror_direction, lst_imgs):
     '''
     Mirrors image left or right, based on criteria specified.
 
     INPUT
         file_path: file path to the folder containing images.
         mirror_direction: criteria for mirroring left or right.
+        lst_imgs: list of image strings.
 
     OUTPUT
         Images mirrored left or right.
     '''
-    img = cv2.imread('file_path' + 'image')
-    img = cv2.flip(img, 1)
-    cv2.imwrite('file_path', img)
-    pass
+
+    for l in lst_imgs:
+        img = cv2.imread(file_path + str(l) + '.jpeg')
+        img = cv2.flip(img, 1)
+        cv2.imwrite(file_path + str(l) + '_mir' + '.jpeg', img)
+
 
 if __name__ == '__main__':
     trainLabels = pd.read_csv("../labels/trainLabels_master.csv")
@@ -48,4 +52,7 @@ if __name__ == '__main__':
     lst_imgs_no_DR = [i for i in trainLabels_no_DR['image']]
     lst_imgs_DR = [i for i in trainLabels_DR['image']]
 
-    
+    lst_sample = [i for i in os.listdir('../data/sample/') if i != '.DS_Store']
+    lst_sample = [str(l.strip('.jpeg')) for l in lst_sample]
+
+    mirror_images('../data/sample-resized/', 1, lst_sample)
