051e3a2a28bae59fc43536aa6917c4ee5c419985
gregwchase <gregory.chase@colorado.edu>
2017-09-24 22:40:28 -0600

    Added rotate_images function

--- src/rotate_images.py
+++ src/rotate_images.py
@@ -4,8 +4,9 @@ from skimage import io
 from skimage.transform import rotate
 import cv2
 import os
+import time
 
-def rotate_image(file_path, degrees_of_rotation):
+def rotate_images(file_path, degrees_of_rotation, lst_imgs):
     '''
     Rotates image based on a specified amount of degrees
 
@@ -13,15 +14,17 @@ def rotate_image(file_path, degrees_of_rotation):
         file_path: file path to the folder containing images.
         degrees_of_rotation: Integer, specifying degrees to rotate the
         image. Set number from 1 to 360.
+        lst_imgs: list of image strings.
 
     OUTPUT
         Images rotated by the degrees of rotation specififed.
     '''
 
-    img = io.imread('../data/sample/' + 'image_path_here')
-    img = rotate(img, 90)
-    io.imsave('img_name + _r_ + 90/180 + .jpeg', img)
-    pass
+    for l in lst_imgs:
+        img = io.imread(file_path + str(l) + '.jpeg')
+        img = rotate(img, degrees_of_rotation)
+        io.imsave(file_path + str(l) + '_' + str(degrees_of_rotation) + '.jpeg', img)
+
 
 def mirror_images(file_path, mirror_direction, lst_imgs):
     '''
@@ -43,6 +46,7 @@ def mirror_images(file_path, mirror_direction, lst_imgs):
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     trainLabels = pd.read_csv("../labels/trainLabels_master.csv")
 
     trainLabels['image'] = trainLabels['image'].str.rstrip('.jpeg')
@@ -52,7 +56,30 @@ if __name__ == '__main__':
     lst_imgs_no_DR = [i for i in trainLabels_no_DR['image']]
     lst_imgs_DR = [i for i in trainLabels_DR['image']]
 
-    lst_sample = [i for i in os.listdir('../data/sample/') if i != '.DS_Store']
-    lst_sample = [str(l.strip('.jpeg')) for l in lst_sample]
+    # lst_sample = [i for i in os.listdir('../data/sample/') if i != '.DS_Store']
+    # lst_sample = [str(l.strip('.jpeg')) for l in lst_sample]
 
-    mirror_images('../data/sample-resized/', 1, lst_sample)
+
+    # Mirror Images with no DR one time
+    print("Mirroring Non-DR Images")
+    mirror_images('../data/train-resized-256/', 1, lst_imgs_no_DR)
+
+
+    # Rotate all images that have any level of DR
+    print("Rotating 90 Degrees")
+    rotate_images('../data/train-resized-256/', 90, lst_imgs_DR)
+
+    print("Rotating 120 Degrees")
+    rotate_images('../data/train-resized-256/', 120, lst_imgs_DR)
+
+    print("Rotating 180 Degrees")
+    rotate_images('../data/train-resized-256/', 180, lst_imgs_DR)
+
+    print("Rotating 270 Degrees")
+    rotate_images('../data/train-resized-256/', 270, lst_imgs_DR)
+
+    print("Mirroring DR Images")
+    mirror_images('../data/train-resized-256/', 0, lst_imgs_DR)
+
+    print("Completed")
+    print("--- %s seconds ---" % (time.time() - start_time))
