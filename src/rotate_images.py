c9e694d7d5a8bdf88ac29d41c68aef0effda77bf
gregwchase <gregory.chase@colorado.edu>
2017-09-24 18:26:06 -0600

    Initial commit - rotate_images.py

---
+++ src/rotate_images.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+from skimage import io
+from skimage.transform import rotate
+import cv2
+
+def rotate_image(file_path, degrees_of_rotation):
+    '''
+    Rotates image based on a specified amount of degrees
+
+    INPUT
+        file_path: file path to the folder containing images.
+        degrees_of_rotation: Integer, specifying degrees to rotate the
+        image. Set number from 1 to 360.
+
+    OUTPUT
+        Images rotated by the degrees of rotation specififed.
+    '''
+
+    img = io.imread('../data/sample/' + 'image_path_here')
+    img = rotate(img, 90)
+    io.imsave('img_name + _r_ + 90/180 + .jpeg', img)
+    pass
+
+def mirror_images(file_path):
+    '''
+    Mirrors image left or right, based on criteria specified.
+
+    INPUT
+        file_path: file path to the folder containing images.
+        mirror_direction: criteria for mirroring left or right.
+
+    OUTPUT
+        Images mirrored left or right.
+    '''
+    img = cv2.imread('file_path' + 'image')
+    img = cv2.flip(img, 1)
+    cv2.imwrite('file_path', img)
+    pass
+
+if __name__ == '__main__':
+    trainLabels = pd.read_csv("../labels/trainLabels_master.csv")
+
+    trainLabels['image'] = trainLabels['image'].str.rstrip('.jpeg')
+    trainLabels_no_DR = trainLabels[trainLabels['level'] == 0]
+    trainLabels_DR = trainLabels[trainLabels['level'] >= 1]
+
+    lst_imgs_no_DR = [i for i in trainLabels_no_DR['image']]
+    lst_imgs_DR = [i for i in trainLabels_DR['image']]
+
+    
