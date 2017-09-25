70f3ccde7c70f24207d60148c20aa9bff3200b07
gregwchase <gregory.chase@colorado.edu>
2017-09-25 12:23:17 -0600

    Changed file directories to point at training data folder.

--- src/reconcile_labels.py
+++ src/reconcile_labels.py
@@ -2,29 +2,38 @@ import pandas as pd
 import os
 import numpy as np
 
+
+def get_lst_images(file_path):
+    '''
+    Reads in all files from file path into a list.
+
+    INPUT
+        file_path: specified file path containing the images.
+
+    OUTPUT
+        List of image strings
+    '''
+    return [i for i in os.listdir(file_path) if i != '.DS_Store']
+
 if __name__ == '__main__':
 
     trainLabels = pd.read_csv("../labels/trainLabels_master.csv")
-    # trainLabels['image'] = trainLabels['image'].str.rstrip('.jpeg')
 
-    lst_imgs = [i for i in os.listdir('../data/sample-resized-256') if i != '.DS_Store']
+    lst_imgs = get_lst_images('../train/train-resized-256')
 
     new_trainLabels = pd.DataFrame({'image': lst_imgs})
-    # new_trainLabels['level'] = np.nan
     new_trainLabels['image2'] = new_trainLabels.image
 
     # Remove the suffix from the image names.
     new_trainLabels['image2'] = new_trainLabels.loc[:,'image2'].apply(lambda x: '_'.join(x.split('_')[0:2]))
 
-    # Strip and add .jpeg back in
+    # Strip and add .jpeg back into file name
     new_trainLabels['image2'] = new_trainLabels.loc[:,'image2'].apply(lambda x: '_'.join(x.split('_')[0:2]).strip('.jpeg') + '.jpeg')
 
-    trainLabels = trainLabels[0:10]
+    # trainLabels = trainLabels[0:10]
     new_trainLabels.columns = ['train_image_name', 'image']
 
-    # print(trainLabels.head())
-    # print(new_trainLabels.head(20))
 
     trainLabels = pd.merge(trainLabels, new_trainLabels, how='outer', on='image')
     trainLabels.drop(['black'], axis = 1, inplace=True)
-    print(trainLabels)
+    print(trainLabels.head(100))
