bf575eae2f2432605e7a82d61827f3bbe61c8915
Greg Chase <gregorywchase@gmail.com>
2020-01-28 16:58:11 -0700

    Move all functions to PreprocessImages class

--- src/resize_images.py
+++ src/resize_images.py
@@ -8,72 +8,80 @@ import pandas as pd
 from PIL import Image
 from tqdm import tqdm
 
+class PreprocessImages:
 
-def make_image_thumbnail(filename):
-    """
-    Resize image for CNN
+    def __init__(self):
+        self.df_labels = pd.read_csv("../data/trainLabels.csv")
 
-    INPUT
-        filename: str, required; string of image to be processed
-    
-    OUTPUT
-        Resized image, saved to directory
-        String of filename with directory
-    """
-    # The thumbnail will be named "<original_filename>_thumbnail.jpg"
-    base_filename, file_extension = os.path.splitext(filename)
-    thumbnail_filename = f"{base_filename}{file_extension}"
-    
-    thumbnail_filename = thumbnail_filename.replace("train", "train_resized")
+    def make_image_thumbnail(self, filename):
+        """
+        Resize image for CNN
 
-    # Create and save thumbnail image
-    image = Image.open(filename)
-    image.thumbnail(size=(256, 256))
-    image.save(f"{thumbnail_filename}", "jpeg")
-
-    return thumbnail_filename
-
-def process_images():
-    """
-    Create a pool of processes to resize images.
-    One process created per CPU
-    """
-
-    with ProcessPoolExecutor() as executor:
+        INPUT
+            filename: str, required; string of image to be processed
         
-        # Get a list of files to process
-        image_files = glob.glob("../data/train/*.jpeg")
+        OUTPUT
+            Resized image, saved to directory
+            String of filename with directory
+        """
+        # The thumbnail will be named "<original_filename>.jpeg"
+        base_filename, file_extension = os.path.splitext(filename)
+        thumbnail_filename = f"{base_filename}{file_extension}"
+        
+        thumbnail_filename = thumbnail_filename.replace("train", "train_resized")
 
-        # Process the list of files, but split the work across the process pool to use all CPUs
-        zip(image_files, executor.map(make_image_thumbnail, image_files))
+        # Create and save thumbnail image
+        image = Image.open(filename)
+        image.thumbnail(size=(256, 256))
+        image.save(f"{thumbnail_filename}", "jpeg")
 
-def create_directories():
-    """
-    Create directories for images
-    """
+        return thumbnail_filename
 
-    for val in list(range(0,5)):
-        if not os.path.exists(f"../data/train_resized/{val}"):
-            os.mkdir(f"../data/train_resized/{val}")
+    def process_images(self):
+        """
+        Create a pool of processes to resize images.
+        One process created per CPU
+        """
 
-def move_images():
-    """
-    Move images to folder based on label
-    """
+        with ProcessPoolExecutor() as executor:
+            
+            # Get a list of files to process
+            image_files = glob.glob("../data/train/*.jpeg")
 
-    df_labels = pd.read_csv("../data/trainLabels.csv")
+            # Process the list of files, but split the work across the process pool to use all CPUs
+            zip(image_files, executor.map(self.make_image_thumbnail, image_files))
 
-    dict_images = dict(zip(df_labels["image"], df_labels["level"]))
+    def create_directories(self):
+        """
+        Create directories for images
+        """
 
-    # Move images to labeled directory
-    for img in tqdm(dict_images.items()):
-        shutil.move(f"../data/train_resized/{img[0]}.jpeg",
-            f"../data/train_resized/{img[1]}/{img[0]}.jpeg")
+        for val in list(range(0,5)):
+            if not os.path.exists(f"../data/train_resized/{val}"):
+                os.mkdir(f"../data/train_resized/{val}")
+
+    def move_images(self):
+        """
+        Move images to folder based on label
+        """
+
+        dict_images = dict(zip(
+            self.df_labels["image"],
+            self.df_labels["level"]
+            ))
+
+        # Move images to labeled directory
+        for img in tqdm(dict_images.items()):
+            shutil.move(f"../data/train_resized/{img[0]}.jpeg",
+                f"../data/train_resized/{img[1]}/{img[0]}.jpeg")
 
 
 if __name__ == '__main__':
-    process_images()
+    
+    preprocess = PreprocessImages()
 
-    create_directories()
+    preprocess.process_images()
 
-    move_images()
+    preprocess.create_directories()
+
+    preprocess.move_images()
