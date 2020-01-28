9562e357b7c5a170b1225b76774d4d0778c9c19d
Greg Chase <gregorywchase@gmail.com>
2020-01-28 16:08:21 -0700

    Split data before applying class imbalance correction

--- src/correct_class_imbalance.py
+++ src/correct_class_imbalance.py
@@ -8,26 +8,28 @@ if __name__ == '__main__':
 
     df_labels = pd.read_csv("../data/trainLabels.csv")
 
-    le = LabelEncoder()
-
-    le.fit(df_labels["image"])
-
-    X = le.transform(df_labels["image"])
-
-    ros = RandomOverSampler()
-
-    X_resampled, Y_resampled = ros.fit_resample(X.reshape(-1,1), df_labels["level"])
-
-    df_balanced_classes = pd.DataFrame({"image": list(chain(*X_resampled)), "level": Y_resampled})
-
-    # Get name of image from inverse transformation
-    # df_balanced_classes["image"] = le.inverse_transform(df_balanced_classes["image"])
-
-    # Create test/train split
+    # Create train/test data before correcting class imbalance
     # TODO: Remove duplicate images from test set
     # TODO: Make distribution similar to test set?
-    X_train, X_test, Y_train, Y_test = train_test_split(
-        df_balanced_classes["image"],
-        df_balanced_classes["level"],
+    X_train, X_test = train_test_split(df_labels,
         test_size=0.2,
         random_state=42)
+
+    # Create Label Encoder
+    le = LabelEncoder()
+
+    le.fit(X_train["image"])
+
+    X_transform = le.transform(X_train["image"])
+    
+    """
+    Over-sample image data
+    """
+    ros = RandomOverSampler()
+
+    X_resampled, Y_resampled = ros.fit_resample(X_transform.reshape(-1,1), X_train["level"])
+
+    df_balanced_classes = pd.DataFrame({"image": list(chain(*X_resampled)), "level": Y_resampled})
+        
+    # Get image_name from inverse transformation
+    df_balanced_classes["image_name"] = le.inverse_transform(df_balanced_classes["image"])
