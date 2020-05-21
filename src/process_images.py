0a661017e161330d29c1672f36f775906f13a19c
Greg Chase <gregorywchase@gmail.com>
2020-05-21 12:33:59 -0600

    Create ProcessImages class

--- src/process_images.py
+++ src/process_images.py
@@ -2,47 +2,66 @@ import cv2
 import glob
 import numpy as np
 
-scale=300
 
-def scaleRadius(img,scale):
-    
-    x=img[img.shape[0]//2,:,:].sum(1)
-    
-    r=(x>x.mean()/10).sum()/2
-    
-    s=scale * 1.0/r
-    
-    return cv2.resize(img,(0,0),fx=s,fy=s)
+class ProcessImages:
 
-for f in glob.glob("../data/sample/*.jpeg"):
-    try:
-        a=cv2.imread(f)
+    def __init__(self):
+        self.SCALE = 300
+        self.IMG_DIR = "../data/sample/*.jpeg"
 
-        #scale img to a given radius
-        a=scaleRadius(a,scale)
+    def scaleRadius(self, img):
         
-        #subtract local mean color
-        a=cv2.addWeighted(a,
-            4,
-            cv2.GaussianBlur(
-                a,(0,0),scale/30),
-            -4,
-            128)
+        x=img[img.shape[0]//2,:,:].sum(1)
         
-        #remove outer 10%
-        b=np.zeros(a.shape)
-        cv2.circle(b,
-            (a.shape[1]//2,a.shape[0]//2),
-            int(scale * 0.9),(1,1,1),-1,8,0)
+        r=(x>x.mean()/10).sum()/2
         
-        a=a*b+128 * (1-b)
+        s=self.SCALE * 1.0/r
         
-        print(f"Processed {f} successfully")
-        
-        img_name = f.split("/")[-1]
-        
-        # Save processed image
-        cv2.imwrite(f"data/processed/{img_name}",a)
+        return cv2.resize(img,(0,0),fx=s,fy=s)
+
+    def subtract_local_average_color(self):
+        """
+        Remove average local color per image
+        Save image to new directory
+        """
+        for f in glob.glob(self.IMG_DIR):
+            
+            try:
+                a=cv2.imread(f)
+
+                #scale img to a given radius
+                a=self.scaleRadius(a)
+                
+                #subtract local mean color
+                a=cv2.addWeighted(a,
+                    4,
+                    cv2.GaussianBlur(
+                        a,(0,0),self.SCALE/30),
+                    -4,
+                    128)
+                
+                #remove outer 10%
+                b=np.zeros(a.shape)
+                cv2.circle(b,
+                    (a.shape[1]//2,a.shape[0]//2),
+                    int(self.SCALE * 0.9),(1,1,1),-1,8,0)
+                
+                a=a*b+128 * (1-b)
+                
+                print(f"Processed {f} successfully")
+                
+                # Original image name
+                img_name = f.split("/")[-1]
+                
+                # Save processed image
+                cv2.imwrite(f"../data/processed/{img_name}",a)
+            
+            except:
+                print(f"Unable to process {f}")
+
+if __name__ == "__main__":
+
+    preprocess = ProcessImages()
+
+    preprocess.subtract_local_average_color()
     
-    except:
-        print(f"Unable to process {f}")
